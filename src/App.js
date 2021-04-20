d743c5f7450d4e78270b4164dd115e97dbee483f
Arda Erzin <ardaerzin@gmail.com>
2021-04-21 00:02:57 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components'
+import theme from './theme'
+import Foo from './Components/Foo'
+import Bar from './Components/Bar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Foo>
+        hello Foo
+      </Foo>
+      <Bar>
+        hello Bar
+      </Bar>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Components/Foo.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Foo = styled.div`
+
+`
+
+export default Foo
